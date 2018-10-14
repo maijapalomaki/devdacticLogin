7bd7e666da08b38a53031d715d7fac76117cc3b2
maijapalomaki <maija.palomaki@student.laurea.fi>
2018-10-14 19:53:00 +0300

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './public/register/register.module#RegisterPageModule' },
+  {
+    path: 'members',
+    loadChildren: './members/member-routing.module#MemberRoutingModule'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
